7b52f633bbe0f3e528e00e5342c15a0483798b99
Dmitry Chiruhin <chiruhind@gmail.com>
2024-10-07 15:16:30 +0500

    Add interlocutor's messages

--- simple-chat/src/scripts/addMessageOnScreen.js
+++ simple-chat/src/scripts/addMessageOnScreen.js
@@ -1,6 +1,6 @@
 export const addMessageOnScreen = (message, messagesContainer) => {
     let div_message = document.createElement("div");
-    div_message.className = "message";
+    div_message.className = `message ${message.name === 'Я' ? "message-right" : "message-left"}`;
 
     let div_text = document.createElement("div");
     div_text.className = "message__text";
@@ -13,12 +13,15 @@ export const addMessageOnScreen = (message, messagesContainer) => {
     div_time.className = "message__time";
     div_time.innerText = message.time;
 
-    let span_done = document.createElement("span");
-    span_done.className = "material-icons message__status status";
-    span_done.innerText = "done_all";
-
     div_bottom.appendChild(div_time);
-    div_bottom.appendChild(span_done);
+
+    if (message.name === 'Я') {
+        let span_done = document.createElement("span");
+        span_done.className = "material-icons message__status status";
+        span_done.innerText = "done_all";
+        div_bottom.appendChild(span_done);
+    }
+
     div_message.appendChild(div_text);
     div_message.appendChild(div_bottom);
     messagesContainer.appendChild(div_message);
--- simple-chat/src/scripts/localStorage.js
+++ simple-chat/src/scripts/localStorage.js
@@ -4,9 +4,23 @@ export const loadMessagesFromStorage = () => {
     let json = localStorage.getItem(MESSAGES_STORAGE_KEY);
     console.log(localStorage.getItem(MESSAGES_STORAGE_KEY));
     console.log(json === null ? [] : JSON.parse(json))
-    return json === null ? [] : JSON.parse(json);
+    let data = json === null ? [] : JSON.parse(json);
+
+    //Временно для демонстрации сообщений собеседника
+    if (data.length == 0 || data[0].name == 'Я') {
+        data = addInterlocutorMessages(data);
+    }
+    return data;
 }
 
 export const saveMessagesToStorage = (data) => {
     localStorage.setItem(MESSAGES_STORAGE_KEY, JSON.stringify(data));
+}
+
+const addInterlocutorMessages = (data) => {
+    const messages = [
+        {text: "Привет!", name: "Дженнифер Эшли", time: "12:12"},
+        {text: "Как дела?", name: "Дженнифер Эшли", time: "12:12"},
+    ];
+    return [...messages, ...data];
 }
